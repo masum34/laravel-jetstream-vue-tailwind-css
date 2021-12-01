358b69e21c94301e4fe98311b818990e5dd533a5
Masum Biswas <masumbiswas7523@gmail.com>
2021-12-01 21:09:36 +0600

    I installed moment js and added it globally

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import moment from 'moment';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -10,10 +11,19 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => require(`./Pages/${name}.vue`),
     setup({ el, app, props, plugin }) {
-        return createApp({ render: () => h(app, props) })
-            .use(plugin)
-            .mixin({ methods: { route } })
-            .mount(el);
+        const JetstreamVue = createApp({ render: () => h(app, props) })
+        .use(plugin)
+        .mixin({ methods: { route } })
+        JetstreamVue.config.globalProperties.$moment = {
+            FormNow(created){
+                return moment(created).fromNow();
+            },
+            DayMonthYear(time){
+                return moment(time).calendar('DD/MM/YYYY');
+            }
+        };
+        JetstreamVue.mount(el);
+        return JetstreamVue;
     },
 });
 
